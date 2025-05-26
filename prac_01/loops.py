385bf500be8e376ce10e4dd8df3d53a9abd7cc95
Amy1 <ayemyomin@my.jcu.edu.au>
2025-05-26 19:02:23 +0800

    loops

---
+++ prac_01/loops.py
@@ -0,0 +1,14 @@
+#a
+for i in range(0,101,10):
+    print(i)
+#b
+for a in range (20,0,-1):
+    print(a)
+#c
+number=int(input("Enter number: "))
+for i in range(number):
+    print("*", end="")
+#d
+number=int(input("Enter number: "))
+for i in range(1,number+1):
+    print("*" * i)
