85907eb9491e43fa5ddf767b6776d9fe3b3d7b76
Amy1 <ayemyomin@my.jcu.edu.au>
2025-06-01 16:24:04 +0800

    Printing results

--- prac_02/score.py
+++ prac_02/score.py
@@ -2,14 +2,20 @@
 CP1404/CP5632 - Practical
 Broken program to determine score status
 """
+import random
+MINIMUM_SCORE=0
+MAXIMUM_SCORE=100
 
 def main():
     score=float(input("Enter score: "))
     result= validate_score(score)
     print(f"Your result is {result}")
+    random_score = random.uniform(MINIMUM_SCORE, MAXIMUM_SCORE)
+    random_result = validate_score(random_score)
+    print(f"Your random number, {random_score} result is {random_result}")
 
 def validate_score(score):
-    if score < 0 or score > 100:
+    if score < MINIMUM_SCORE or score > MAXIMUM_SCORE:
         message="Invalid score"
     elif score>=90:
         message="Excellent"
