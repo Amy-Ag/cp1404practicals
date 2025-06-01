bcca36142d31064cfb206e4add40cfd2e3a2e225
Amy-Ag <ayemyomin@my.jcu.edu.au>
2025-06-01 16:25:02 +0800

    Delete prac_02/calculate_area.py

--- prac_02/calculate_area.py
+++
@@ -1,6 +0,0 @@
-def calculate_rectangle_area(length,width):
-    """This function will calculate the area of rectangle"""
-    return length*width
-
-area = calculate_rectangle_area(10, 12)
-print(f"The area of rectangle is {area}")
