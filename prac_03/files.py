e1fc6762fe7b4945fe0976a3f9fdb0c8865077c1
Amy1 <ayemyomin@my.jcu.edu.au>
2025-06-07 21:43:42 +0800

    final file

--- prac_03/files.py
+++ prac_03/files.py
@@ -1,5 +1,17 @@
 #1
-name= input("Enter the name: ")
-in_file=open("name.txt","w")
-print(name, file=in_file)
-in_file.close()
+# name= input("Enter the name: ")
+# in_file=open("name.txt","w")
+# print(name, file=in_file)
+# in_file.close()
+
+#2
+# in_file=open("name.txt","r")
+# name=in_file.read().strip()
+# print(f"Hi {name}!")
+
+#3
+with open("numbers.txt","r") as in_file:
+    first_number= int(in_file.readline())
+    second_number= int(in_file.readline())
+result=first_number+second_number
+print(result)
