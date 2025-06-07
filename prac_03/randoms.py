af85168fd3b1438b89741a2fdf9b42db0ccde4f9
Amy1 <ayemyomin@my.jcu.edu.au>
2025-06-07 15:03:33 +0800

    random function

---
+++ prac_03/randoms.py
@@ -0,0 +1,19 @@
+# What did you see on line 1?
+#Python console generated 19.
+# What was the smallest number you could have seen, what was the largest?
+#Smallest: 5 and Largest: 20
+
+# What did you see on line 2?
+#Python console generated a 3.
+# What was the smallest number you could have seen, what was the largest?
+#Smallest: 3 and Largest: 9
+# Could line 2 have produced a 4?
+#The possible values are: 3,5,7,9. Therefore, line 2 wouldn't produce a 4.
+
+# What did you see on line 3?
+#Python console generated 3.23261792940845
+# What was the smallest number you could have seen, what was the largest?
+#Smallest: just a little above 2.5 and Largest: just below or equal to 5.5
+
+import random
+print(random.randint(1, 100))
