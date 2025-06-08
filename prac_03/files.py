3b92bb48f528f57ea2ed7bbccb06fa31d9d469d1
Amy1 <ayemyomin@my.jcu.edu.au>
2025-06-08 12:23:40 +0800

    file readings

--- prac_03/files.py
+++ prac_03/files.py
@@ -1,13 +1,13 @@
 #1
-# name= input("Enter the name: ")
-# in_file=open("name.txt","w")
-# print(name, file=in_file)
-# in_file.close()
+name= input("Enter the name: ")
+in_file=open("name.txt","w")
+print(name, file=in_file)
+in_file.close()
 
 #2
-# in_file=open("name.txt","r")
-# name=in_file.read().strip()
-# print(f"Hi {name}!")
+in_file=open("name.txt","r")
+name=in_file.read().strip()
+print(f"Hi {name}!")
 
 #3
 with open("numbers.txt","r") as in_file:
@@ -15,3 +15,10 @@ with open("numbers.txt","r") as in_file:
     second_number= int(in_file.readline())
 result=first_number+second_number
 print(result)
+
+#4
+total=0
+with open("numbers.txt","r") as in_file:
+    for line in in_file:
+        total += int(line)
+print(total)
