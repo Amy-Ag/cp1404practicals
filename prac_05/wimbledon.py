7956666f9bd9d8289ece75dc614d30aff9ba9796
Amy1 <ayemyomin@my.jcu.edu.au>
2025-06-21 21:38:57 +0800

    records function loaded

--- prac_05/wimbledon.py
+++ prac_05/wimbledon.py
@@ -3,6 +3,8 @@ Wimbledon
 Estimated: 45 minutes
 Actual:
 """
+from logging import LogRecord
+
 FILENAME= "wimbledon.csv"
 COUNTRY_INDEX=1
 CHAMPION_INDEX=2
@@ -24,3 +26,19 @@ def clean_record(records):
         except KeyError:
             champion_count[record[COUNTRY_INDEX]] = 1
     return champion_count, countries
+
+def display_result(champion_count, countries):
+    """Print champions and countries."""
+    print("Wimbledon Champions: ")
+    for name, count in champion_count.items():
+        print(name,count)
+    print(f"\nThese {len(countries)} countries have won Wimbledon: ")
+    print(" ".join(sorted(countries)))
+
+def get_records(filename):
+    records = []
+    with open(filename, "r", encoding="utf-8-sig") as in_file:
+        in_file.readline()
+        for line in in_file:
+            records.append(line.strip().split(","))
+        return records
