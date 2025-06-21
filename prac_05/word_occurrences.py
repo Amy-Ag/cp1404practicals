a4ded9f8494e05a8007793e074316d7b2f211ff1
Amy1 <ayemyomin@my.jcu.edu.au>
2025-06-21 12:14:04 +0800

    dictionary created& each word accumulated

--- prac_05/word_occurrences.py
+++ prac_05/word_occurrences.py
@@ -1,6 +1,12 @@
 """
 Word Occurrences
 Estimate: 30-40 minutes
-Actual:
+Actual: 16 minutes
 """
+text=input("Text: ")
+words=text.split()
+words_to_count={}
+for word in words:
+    words_to_count[word]= words_to_count.get(word, 0) + 1
+sorted_words=sorted(words_to_count.items())
 
