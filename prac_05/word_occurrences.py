abec5a9cdcfd68c6800306b4fb72df67bbda2cf6
Amy1 <ayemyomin@my.jcu.edu.au>
2025-06-21 12:14:35 +0800

    Complete prac5 word occurrences

--- prac_05/word_occurrences.py
+++ prac_05/word_occurrences.py
@@ -9,4 +9,6 @@ words_to_count={}
 for word in words:
     words_to_count[word]= words_to_count.get(word, 0) + 1
 sorted_words=sorted(words_to_count.items())
-
+maximum_word_length=max(len(word) for word, count in sorted_words)
+for word,count in sorted_words:
+    print(f"{word:{maximum_word_length}} : {count}")
