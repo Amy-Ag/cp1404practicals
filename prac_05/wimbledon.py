6305e55e149c3ee8896f4292e3059c54704acd3d
Amy1 <ayemyomin@my.jcu.edu.au>
2025-06-21 21:30:23 +0800

    record sorted and cleaned

--- prac_05/wimbledon.py
+++ prac_05/wimbledon.py
@@ -13,3 +13,14 @@ def main():
     champion_count, countries= clean_record(records)
     display_result(champion_count, countries)
 
+def clean_record(records):
+    """Create dictionary to store champions and countries."""
+    champion_count = {}
+    countries = set()
+    for record in records:
+        countries.add(record[COUNTRY_INDEX])
+        try:
+            champion_count[record[COUNTRY_INDEX]] += 1
+        except KeyError:
+            champion_count[record[COUNTRY_INDEX]] = 1
+    return champion_count, countries
