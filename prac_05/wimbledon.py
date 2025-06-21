f45cc218d0ddc4645a39c55b829d96d61a83eb45
Amy1 <ayemyomin@my.jcu.edu.au>
2025-06-21 21:28:45 +0800

    main created

--- prac_05/wimbledon.py
+++ prac_05/wimbledon.py
@@ -1,5 +1,15 @@
 """
 Wimbledon
-Estimated: 30-35 minutes
+Estimated: 45 minutes
 Actual:
 """
+FILENAME= "wimbledon.csv"
+COUNTRY_INDEX=1
+CHAMPION_INDEX=2
+
+def main():
+    """Display clean data for Wimbledon champions."""
+    records= get_records(FILENAME)
+    champion_count, countries= clean_record(records)
+    display_result(champion_count, countries)
+
