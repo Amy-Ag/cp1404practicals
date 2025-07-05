ddd944ae70829d6710dfc6397875196864be6103
Amy1 <ayemyomin@my.jcu.edu.au>
2025-07-05 22:40:14 +0800

    wrote methods in class

---
+++ prac_06/programming_language.py
@@ -0,0 +1,16 @@
+"""
+Estimated: 30 minutes
+Actual: mins
+"""
+
+class ProgrammingLanguage:
+    def __init__(self, language, typing, reflection, year=0):
+        self.language = language
+        self.typing = typing
+        self.reflection = reflection
+        self.year = year
+
+    def is_dynamic(self):
+        return self.typing.lower() == "dynamic"
+    def __str__(self):
+        return f"{self.language}, {self.typing} Typing, Reflection={self.reflection}, First appeared in {self.year}"
