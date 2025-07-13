b190b8740132f5578df2a027135611f0d6b44399
Amy1 <ayemyomin@my.jcu.edu.au>
2025-07-13 14:00:51 +0800

    prompted user to add new guitars and saved to csv file

--- prac_07/myguitars.py
+++ prac_07/myguitars.py
@@ -6,6 +6,10 @@ def main():
     guitars = load_guitars(FILENAME)
     guitars.sort()
     display_guitars(guitars)
+    new_guitars=add_new_guitar()
+    for guitar in new_guitars:
+        guitars.append(guitar)
+    save_guitars(guitars, FILENAME)
 
 def load_guitars(filename):
     """Load guitar from the csv file into list of Guitar objects."""
@@ -24,6 +28,25 @@ def display_guitars(guitars):
     print("These are my guitars: ")
     for i, guitar in enumerate(guitars,1):
         print(guitar)
+
+def add_new_guitar():
+    """Prompt user to add a new guitar."""
+    guitars=[]
+    print("Would you like to add a new guitar?")
+    name= input("Name:")
+    while name != "":
+        year = int(input("Year:"))
+        cost = float(input("Cost:"))
+        guitars.append(Guitar(name, year, cost))
+        name = input("Name:")
+    return guitars
+
+
+def save_guitars(guitars, filename):
+    """Save the guitar list."""
+    with open(filename, "w") as out_file:
+        for guitar in guitars:
+            print(f"{guitar.name},{guitar.year},{guitar.cost}", file=out_file)
 
 if __name__ == '__main__':
     main()
