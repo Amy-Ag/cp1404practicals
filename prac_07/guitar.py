deec08713eecdc19e1c0d71acb43290fb804cc4a
Amy1 <ayemyomin@my.jcu.edu.au>
2025-07-13 13:32:24 +0800

    less than function added

---
+++ prac_07/guitar.py
@@ -0,0 +1,27 @@
+"""
+Estimated: 40 minutes
+Actual: 30mins
+"""
+
+class Guitar:
+    """Represent guitar object."""
+    def __init__(self, name="", year=0, cost=0):
+        """Initialize guitar instance."""
+        self.name = name
+        self.year = year
+        self.cost = cost
+
+    def __str__(self):
+        """Convert into string."""
+        return f"{self.name} ({self.year}) : ${self.cost:,.2f}"
+
+    def get_age(self, year):
+        """Return age in years."""
+        return  year - self.year
+
+    def is_vintage(self, year):
+        """Return True if guitar is more than 50 y/o."""
+        return self.get_age(year)>=50
+
+    def __lt__(self, other):
+        return self.year < other.year
