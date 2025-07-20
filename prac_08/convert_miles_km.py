5b656fa1271bb026f4cba13063491b7985e2aead
Amy1 <ayemyomin@my.jcu.edu.au>
2025-07-20 15:20:52 +0800

    docstrings added

--- prac_08/convert_miles_km.py
+++ prac_08/convert_miles_km.py
@@ -1,30 +1,32 @@
 from kivy.app import App
 from kivy.lang import Builder
+from kivy.properties import StringProperty
+
 
 MILES_TO_KM=1.60934
 
 class ConvertMiles(App):
-    """."""
+    """App to convert miles to km."""
     def build(self):
-        """."""
+        """Initialize output text and build GUI from KV layout."""
         self.title = "Convert Miles to Kilometres"
         self.root = Builder.load_file('convert_m_km_solution.kv')
         return self.root
 
     def handle_calculate(self):
-        """."""
+        """Convert miles to km."""
         value = self.get_validated_miles()
         result = value * MILES_TO_KM
         self.root.ids.output_label.text = str(result)
 
     def handle_increment(self, change):
-        """."""
+        """Increment value in the input and recalculate."""
         value = self.get_validated_miles() + change
         self.root.ids.input_miles.text = str(value)
         self.handle_calculate()
 
     def get_validated_miles(self):
-        """."""
+        """Return float value from input or print 0 if invalid."""
         try:
             value = float(self.root.ids.input_miles.text)
             return value
