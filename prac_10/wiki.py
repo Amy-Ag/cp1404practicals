400d563ec5138f5f70df891c15f30a723dc61cd5
Amy1 <ayemyomin@my.jcu.edu.au>
2025-08-02 17:43:14 +0800

    modified required changes

--- prac_10/wiki.py
+++ prac_10/wiki.py
@@ -5,14 +5,14 @@ def main():
     while title != "":
         try:
             page = wikipedia.page(title, auto_suggest=False)
-            print("Title:", page.title)
-            print("URL: ", page.url)
-            print("Summary: ", page.content)
+            print(page.title)
+            print(page.content)
+            print(page.url)
         except wikipedia.exceptions.DisambiguationError as e:
             print("DisambiguationError: ", e.options[:5])
         except wikipedia.exceptions.PageError:
             print("Page not found.")
         title = input("\nEnter page title or search phrase: ").strip()
-
+    print("Thank you.")
 
 main()
